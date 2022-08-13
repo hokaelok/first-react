0fad8bff7d4cd6e097b0382dd9c6249bbced7b45
Ho Kae Lok <hokaelok123@gmail.com>
2022-08-13 12:48:19 +0800

    Push to master (13 Aug 2022 12:48)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { FirstForm } from "./components";
 
 const App = () => {
--- src/components/FirstForm.js
+++ src/components/FirstForm.js
@@ -7,10 +7,9 @@ export default function FirstForm() {
         email: "",
         comment: "",
         isFriendly: true,
+        employment: "",
+        favColor: "",
     });
-
-    console.log(formData);
-    console.log(`${formData.firstName} ${formData.lastName} ${formData.email}`);
 
     // on change function
     function handleChange(event) {
@@ -23,8 +22,16 @@ export default function FirstForm() {
         });
     }
 
+    // on submit function
+    function handleSubmit(event) {
+        event.preventDefault();
+        // submitToApi(formData);
+        console.log(formData);
+    }
+
     return (
-        <form className="mx-5">
+        <form className="mx-5" onSubmit={handleSubmit}>
+            {/* input text */}
             <input
                 className="border-2 p-1"
                 type="text"
@@ -41,6 +48,7 @@ export default function FirstForm() {
                 value={formData.lastName}
                 onChange={handleChange}
             />
+            {/* input email */}
             <input
                 className="border-2 m-5 p-1"
                 type="email"
@@ -49,6 +57,7 @@ export default function FirstForm() {
                 value={formData.email}
                 onChange={handleChange}
             />
+            {/* Textarea */}
             <textarea
                 className="border-2 m-5 p-1"
                 name="comment"
@@ -56,6 +65,7 @@ export default function FirstForm() {
                 onChange={handleChange}
             />
             <br />
+            {/* checkbox */}
             <input
                 type="checkbox"
                 name="isFriendly"
@@ -66,6 +76,73 @@ export default function FirstForm() {
                 Are you friendly?
             </label>
             <br />
+            <br />
+            {/* radio buttons */}
+            <fieldset>
+                <legend className="font-bold">Current employment status</legend>
+
+                <input
+                    type="radio"
+                    name="employment"
+                    value="unemployed"
+                    id="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label className="mx-3" htmlFor="unemployed">
+                    Unemployed
+                </label>
+                <br />
+
+                <input
+                    type="radio"
+                    name="employment"
+                    value="part-time"
+                    id="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label className="mx-3" htmlFor="part-time">
+                    Part-time
+                </label>
+                <br />
+
+                <input
+                    type="radio"
+                    name="employment"
+                    value="full-time"
+                    id="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label className="mx-3" htmlFor="full-time">
+                    Full-time
+                </label>
+                <br />
+            </fieldset>
+            <br />
+
+            {/* select / option dropdown box */}
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select
+                name="favColor"
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+            >
+                <option value="">Choose a color</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
+            <br />
+            <br />
+            <button type="submit">Submit</button>
         </form>
     );
 }
